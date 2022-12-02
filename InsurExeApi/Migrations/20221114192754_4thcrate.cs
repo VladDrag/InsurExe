0f41c77db1a8b9fc22656ff4e7a16c403aaee50b
VladDrag <vdragomir@protonmail.com>
2022-12-02 11:27:50 +0100

    Add Docker volume command. First working mini-service.

---
+++ InsurExeApi/Migrations/20221114192754_4thcrate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsurExeApi.Migrations
+{
+    public partial class _4thcrate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- InsurExeApi/Program.cs
+++ InsurExeApi/Program.cs
@@ -2,11 +2,16 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<InsuranceContext>(options =>
-        options.UseSqlite(builder.Configuration.GetConnectionString("InsuranceMovieContext") ?? throw new InvalidOperationException("Connection string 'InsuranceMovieContext' not found.")));
+        options.UseSqlite(builder.Configuration.GetConnectionString("InsuranceContext") ?? throw new InvalidOperationException("Connection string 'InsuranceMovieContext' not found.")));
 
 // Add services to the container.
+// docker build -t insurexedockimage .
+// docker run -it -p 3000:80 -v /Users/vladdragomir/Desktop/SALT/CS/InsurExe/DummyDatabase:/DummyDatabase insurexedockimage -> the mounting must be made in accordance to APPSETTINGS.JSON, meaning that
+// if in appsettings the path to the database is ../db_folder/, then that must mimic the structure of the docker file, which starts at / (before the app directory);
+// in our case, the path ../DummyDatabase means that we must mount the Database in the  /, instead of the /app path
 
 // https://localhost:7296/swagger/index.html for swagger testing
+//dotnet run
 
 builder.Services.AddControllers();
 
